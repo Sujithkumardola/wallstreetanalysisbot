2433ff2e3ad78f7d3bb1070e6ea2a2a839de0c34
sujithkumardola <sujithkumardola33@gmail.com>
2021-11-13 12:31:05 +0530

    Update main.php

--- main.php
+++ main.php
@@ -1,1 +1,24 @@
 
+<?php
+
+$token = '2105277243:AAEstwP_0UNazeLM1AXZf3gE22eRxnZEC-4';
+$link = 'https://api.telegram.org:443/bot'.$token.'';
+
+$getupdate = file_get_contents('php://input'); // for webhook
+
+$responsearray = json_decode($getupdate, TRUE);
+
+$chatid = $responsearray['message']['chat']['id'];
+
+$message = 'Auto Message';
+
+$parameter = array(
+		'chat_id' => $chatid, 
+		'text' => $message
+		);
+
+$request_url = $link.'/sendMessage?'.http_build_query($parameter); 
+
+file_get_contents($request_url);
+
+?>
